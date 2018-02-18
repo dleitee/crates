33c115fb611de326ff31ff14f2877240cb808fcb
Daniel Leite de Oliveira <dleitee@gmail.com>
2018-02-18 18:18:03 -0300

    feat(parser): Add plugins to babylon parser.

--- src/parser.js
+++ src/parser.js
@@ -4,10 +4,12 @@ const parser = async (code, options = {}) => {
   const defaultOptions = {
     sourceType: 'module',
     moduleName: 'crates',
+    plugins: [],
     ...options,
   }
   const ast = getASTFromCode(code, {
     sourceType: defaultOptions.sourceType,
+    plugins: ['jsx', 'flow', 'flowComments', 'typescript', ...defaultOptions.plugins],
   })
   const moduleIdentifier = await getModuleIdentifier(ast, defaultOptions.moduleName)
   return getTranslationsMap(ast, moduleIdentifier)
