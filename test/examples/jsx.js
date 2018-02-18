c3ce7f1ce3f5f87455c605d81cf9b9f16ae61d90
Daniel Leite de Oliveira <dleitee@gmail.com>
2018-02-18 18:29:45 -0300

    test(parser): Add tests for JSX files.

---
+++ test/examples/jsx.js
@@ -0,0 +1,10 @@
+import crates from '../../'
+
+const component = (
+  <div>
+    <span>{crates('Welcome')}</span>
+    <span>{crates('Bem Vindo')}</span>
+  </div>
+)
+
+export default component
---
+++ test/parser.jsx.test.js
@@ -0,0 +1,33 @@
+import fs from 'fs'
+import path from 'path'
+
+import { base64decode } from 'strman'
+
+import parser from '../src/parser'
+
+describe('run parser on JSX file', () => {
+  let file = null
+
+  beforeAll(() => {
+    file = fs.readFileSync(path.resolve(__dirname, 'examples', 'jsx.js'), {
+      encoding: 'UTF-8',
+    })
+  })
+
+  test('to test, the translations must have 1 or more elements', async () => {
+    const translations = await parser(file, { moduleName: '../../' })
+    expect(translations.size).toBeGreaterThan(1)
+  })
+
+  test('parser function should return a map with strings to translate', async () => {
+    const translations = await parser(file, { moduleName: '../../' })
+    expect(translations).toBeInstanceOf(Map)
+  })
+
+  test("the map's keys must be a base64 of value", async () => {
+    const translations = await parser(file, { moduleName: '../../' })
+    translations.forEach((value, key) => {
+      expect(base64decode(key)).toEqual(value)
+    })
+  })
+})
