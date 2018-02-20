765691c1c2d7027311ef6a5158127bdfc380daaf
Daniel Leite de Oliveira <dleitee@gmail.com>
2018-02-19 21:40:13 -0300

    feat(generator): Generate translation file for each language.

---
+++ src/generator.js
@@ -0,0 +1,62 @@
+import fs from 'fs'
+import path from 'path'
+
+import { parse } from 'babylon'
+import generateCode from '@babel/generator'
+import _get from 'lodash.get'
+
+import parser from './parser'
+
+const readFile = filename => fs.readFileSync(filename, { encoding: 'UTF-8' })
+
+const createFolder = pathname => !fs.existsSync(pathname) && fs.mkdirSync(pathname)
+
+const writeFile = (filename, data) => {
+  if (fs.existsSync(filename)) {
+    fs.unlinkSync(filename)
+  }
+  fs.writeFileSync(filename, data)
+}
+
+const getCurrentJSON = (generatedFileName, options = {}) => {
+  const filename = path.resolve(process.cwd(), options.path, generatedFileName)
+  if (fs.existsSync(filename)) {
+    // eslint-disable-next-line
+    return require(filename).default
+  }
+  return {}
+}
+
+const mergeMaps = filesMapped =>
+  filesMapped.reduce((previous, current) => new Map([...previous, ...current]), new Map())
+
+const generateCodeToLanguage = (translationMap, language, options = {}) => {
+  const generatedFileName = `translation.${language}.js`
+  const json = getCurrentJSON(generatedFileName, options)
+  let object = {}
+  translationMap.forEach((value, key) => {
+    const item = {
+      [key]: {
+        phrase: value,
+        translation: _get(json, `${key}.translation`, ''),
+      },
+    }
+    object = { ...object, ...item }
+  })
+  const code = `export default ${JSON.stringify(object)}`
+  const ast = parse(code, { sourceType: 'module' })
+  writeFile(path.resolve(options.path, generatedFileName), generateCode(ast).code)
+}
+
+const generator = async (files = [], languages = [], options = {}) => {
+  const defaultOptions = {
+    path: './translations',
+    ...options,
+  }
+  const filesMapped = await Promise.all(files.map(async file => parser(readFile(file), options)))
+  const allTranslations = mergeMaps(filesMapped)
+  createFolder(defaultOptions.path)
+  languages.forEach(value => generateCodeToLanguage(allTranslations, value, defaultOptions))
+}
+
+export default generator
---
+++ test/generator.test.js
@@ -0,0 +1,29 @@
+import fs from 'fs'
+import path from 'path'
+
+import rimraf from 'rimraf'
+
+import generator from '../src/generator'
+
+describe('run generator through the files', () => {
+  let firstFile = null
+  let secondFile = null
+
+  beforeAll(() => {
+    firstFile = path.resolve(__dirname, 'examples', 'jsx.js')
+    secondFile = path.resolve(__dirname, 'examples', 'vanilla.module.js')
+  })
+
+  test('should generate three files', async () => {
+    const dir = '.tmp'
+    await generator([firstFile, secondFile, secondFile], ['en', 'pt', 'es'], {
+      path: dir,
+      moduleName: '../../',
+    })
+
+    const trueDir = path.resolve(process.cwd(), dir)
+    const filesOnDir = fs.readdirSync(trueDir)
+    expect(filesOnDir).toEqual(['translation.en.js', 'translation.es.js', 'translation.pt.js'])
+    rimraf(trueDir, () => {})
+  })
+})
